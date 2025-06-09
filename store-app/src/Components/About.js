3bdafd1bd1b774105b5f0019d9393333d9aabaff
Alina Akchurina <ohhalina13@gmail.com>
2025-06-09 22:45:33 +0300

    commit

---
+++ store-app/src/Components/About.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import AboutBackground from "../Assets/about-background.png"
+import AboutBackgroundImage from "../Assets/about-background-image.png"
+import { BsFillPlayCircleFill } from 'react-icons/bs'
+
+const About = () => {
+  return (
+    <div className='about-section-container'>
+        <div className="about-background-image-container">
+            <img src={AboutBackground} alt="" />
+        </div>
+        <div className="about-section-image-container">
+            <img src={AboutBackgroundImage} alt="" />
+        </div>
+        <div className="about-section-text-container">
+            <p className="primary-subheading">
+                About
+            </p>
+            <h1 className="primary-heading">
+                Food Is An Important Part Of A Balanced Diet
+            </h1>
+            <p className="primary-text">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda error minima suscipit voluptate harum quia esse incidunt accusantium, dicta rerum, accusamus amet, ipsam necessitatibus omnis natus ab explicabo blanditiis. Earum?
+            </p>
+            <p className="primary-text">
+                Non tincidunt magna non et elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+            </p>
+            <div className="about-buttons-container">
+                <button className="secondary-button">Learn more</button>
+                <button className="watch-wideo-button">
+                    {" "}
+                    <BsFillPlayCircleFill /> Watch Video
+                </button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
