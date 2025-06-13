2627e4d228ecd9ba9bf8c7f59d205c0f93ac7708
Alina Akchurina <ohhalina13@gmail.com>
2025-06-13 14:54:48 +0300

    emailjs

--- store-app/src/Components/Contact.js
+++ store-app/src/Components/Contact.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(
+      'service_app', //тут servise
+      'template_h6dgep8', //тут template
+      form.current,
+      'Z7YvmC2urJfo3hc9P'
+    )
+    .then((result) => {
+      alert('send');
+      form.current.reset();
+    }, (error) => {
+      alert('error send' + error.text);
+    });
+  };
+  
   return (
     <div className="contact-page-wrapper">
         <h1 className="primary-heading">Have Question In Mind?</h1>
         <h1 className="primary-heading">Let Us Help You</h1>
-        <div className="contact-form-container">
-            <input type="text" placeholder='yourmail@gmail.com' />
-            <button className="secondary-button">Submit</button>
+        <form ref={form} onSubmit={sendEmail} className="contact-form-container">
+            <input type="email" name="user_email" placeholder='yourmail@gmail.com' required />
+            <button type="submit" className="secondary-button">Submit</button>
+        </form>
         </div>
-    </div>
   )
 }
 
